57d4e976e95a54b9f8048ca1737be0e58566fdb6
Jamie <Jamielukeoconnell@gmail.com>
2019-09-26 16:33:16 +1000

    Added inital login and register functions

---
+++ palmtree/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), index=True,
+                         unique=True, nullable=False)
+    email = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return "<Name: {}, ID: {}>".format(self.name, self.id)
