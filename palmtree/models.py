77740915d067b214009d755fde0c19be84d63ed5
KeeganHague <55681382+KeeganHague@users.noreply.github.com>
2019-10-01 20:44:52 +1000

    fixed an error in the carDetails

--- palmtree/models.py
+++ palmtree/models.py
@@ -12,3 +12,15 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<Name: {}, ID: {}>".format(self.name, self.id)
+
+class carDetails(db.Model):
+    __tablename__='Car'
+    carID = db.Column(db.Integer, primary_key=True)
+    userID= db.Column(db.Integer, db.ForeignKey('hotels.id'))
+    listingTitle = db.Column(db.String(250), nullable=False)
+    carMake = db.Column(db.String(250  ), nullable=False)
+    picture = db.Column(db.String(250  ), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return "<carID: {}, userID: {}, listingTitle: {}, carMake: {}, picture: {}, price: {}"
