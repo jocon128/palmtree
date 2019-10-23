a5a121b05c707f90150c34a34a9a1062f8ec323e
alexwarton <55681590+alexwarton@users.noreply.github.com>
2019-10-23 18:54:41 +1100

    Sorry

--- palmtree/manage.py
+++ palmtree/manage.py
@@ -13,10 +13,16 @@ bp = Blueprint('manage', __name__)
 
 # #create a page that will show the details for the destination
 
+
+
 @bp.route('/manage', methods=['GET', 'POST'])
 def manage():
     form = ListingForm()
+    listing = Listing.query.all()
     print('Method type: ', request.method)
-    
+    listing = Listing.query.filter_by(carID=1).first()
+    if(form.validate_on_submit()):
+        carMake = form.carMake.data
 
-    return render_template('manage.html', form=form)
+    return render_template('manage.html', form=form, listing=listing)
+
