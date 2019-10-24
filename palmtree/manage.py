21bf241b935e4ccfe453939c0ab4c3d3fde9275d
Jamie <Jamielukeoconnell@gmail.com>
2019-10-24 16:21:13 +1000

    Fixed hard coded /manage id to dynamic

--- palmtree/manage.py
+++ palmtree/manage.py
@@ -14,15 +14,14 @@ bp = Blueprint('manage', __name__)
 # #create a page that will show the details for the destination
 
 
-
-@bp.route('/manage', methods=['GET', 'POST'])
-def manage():
+@bp.route('/manage/<id>', methods=['GET', 'POST'])
+def manage(id):
     form = ListingForm()
     listing = Listing.query.all()
     print('Method type: ', request.method)
-    listing = Listing.query.filter_by(id=1).first()
+    listing = Listing.query.filter_by(id=id).first()
     if request.method == "POST":
-        
+
         listing.title = form.title.data
 
         listing.description = form.description.data
@@ -34,4 +33,3 @@ def manage():
         db.session.commit()
 
     return render_template('manage.html', form=form, listing=listing)
-
